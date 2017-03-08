f88e1b3712a53256dfd72daa4cd43014f5ccfe1a
konrad.hanus <konrad.hanus@gmail.com>
2017-03-08 15:16:42 +0100

    Add first file

---
+++ app.jsx
@@ -0,0 +1,16 @@
+const App = React.createClass({
+
+	render: function(){
+		return (
+		  <div> Hello World</div>
+		)
+	}
+})
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
